2c3995279af46fdb77b6d90275ea0e2b32919071
Siratee K <sirateek@gmail.com>
2022-04-21 17:29:00 +0700

    Add the setter for the x,y attr in the entity

--- src/Entity.java
+++ src/Entity.java
@@ -17,4 +17,12 @@ public abstract class Entity {
     public int getY() {
         return this.y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
