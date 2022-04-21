524f5a1efec6ce89e5fbaee113aa0628dfb5fc3b
Siratee K <sirateek@gmail.com>
2022-04-21 17:29:26 +0700

    Add the Snake Object reperesting the snake and its tails

---
+++ src/Snake.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake extends Entity {
+    public static int INITIAL_TAIL_SIZE = 3;
+    private int dx;
+    private int dy;
+
+    private List<SnakeTail> tails = new ArrayList<SnakeTail>();
+
+    public Snake(int x, int y) {
+        super(x, y);
+        // Always start moving to the right first
+        dx = 1;
+        dy = 0;
+
+        // Start with tails from the configured value;
+        for (int i = 1; i < INITIAL_TAIL_SIZE + 1; i++) {
+            tails.add(new SnakeTail(x, y - i));
+        }
+
+    }
+
+    public void move() {
+        // TODO: Implement the move
+        // 1. Update the current possition with dx,dy;
+        // 2. Update the entire tails
+    }
+}
