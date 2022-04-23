02579fbe205a386543b0ac1fe65958104700aac9
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2022-04-23 22:26:10 +0700

    remove attribute x and y in Food

--- src/Food.java
+++ src/Food.java
@@ -3,27 +3,16 @@ import java.util.Random;
 public class Food extends Entity{
     Random rand = new Random();
     private int boardSize;
-    private int x;
-    private int y;
 
     public Food(int boardSize) {
         this.boardSize = boardSize;
-        x = rand.nextInt(boardSize);
-        y = rand.nextInt(boardSize);
+        setX(rand.nextInt(boardSize));
+        setY(rand.nextInt(boardSize));
     }
 
     public void generate() {
-        x = rand.nextInt(boardSize);
-        y = rand.nextInt(boardSize);
+        setX(rand.nextInt(boardSize));
+        setY(rand.nextInt(boardSize));
     }
 
-    @Override
-    public int getX() {
-        return x;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
 }
